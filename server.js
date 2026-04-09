99bcc0fd20c9e99a8d03d58f8060cd8c9ebfbca3
SHYANTIKASAHAROY <shyantika2006@gmail.com>
2026-04-10 00:02:41 +0530

    added root route

--- server.js
+++ server.js
@@ -100,6 +100,10 @@ app.post("/chat", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("🚀 Disaster Management Backend is Running!");
+});
+
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
